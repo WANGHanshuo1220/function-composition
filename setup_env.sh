f452c6ccbcda4f92f97d7890630606c8b94c8372
IDreamer <73027141+WANGHanshuo1220@users.noreply.github.com>
2024-03-20 17:52:41 +0800

    Create setup_env.sh

---
+++ setup_env.sh
@@ -0,0 +1,17 @@
+sudo apt-get update --yes
+sudo apt-get upgrade --yes
+sudo apt-get install python3-pip --yes
+sudp apt-get install imagemagick libmagickwand-dev --yes
+sudo apt-get install python3-opencv --yes
+
+sudo pip install -r requirements.txt --yes
+
+mkdir ~/.aws
+touch ~/.aws/config
+echo "[default]" >> ~/.aws/config
+echo "region = ap-northeast-1" >> ~/.aws/config
+
+touch ~/.aws/credentials
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id = AKIATENRELP3YJPFMBFM" >> ~/.aws/credentials
+echo "aws_secret_access_key = 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" >> ~/.aws/credentials
